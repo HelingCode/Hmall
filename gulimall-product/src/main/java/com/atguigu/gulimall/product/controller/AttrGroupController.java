15be85a84455194fe18fc68fd624ea4f597d9d77
HelingCode <929926540@qq.com>
2021-11-07 15:43:26 +0800

    新增商品-获取分类下所有分组以及属性

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/AttrGroupController.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/AttrGroupController.java
@@ -11,6 +11,7 @@ import com.atguigu.gulimall.product.service.AttrAttrgroupRelationService;
 import com.atguigu.gulimall.product.service.AttrService;
 import com.atguigu.gulimall.product.service.CategoryService;
 import com.atguigu.gulimall.product.vo.AttrGroupRelationVo;
+import com.atguigu.gulimall.product.vo.AttrGroupWithAttrsVo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -76,6 +77,15 @@ public class AttrGroupController {
         PageUtils page = attrGroupService.queryPage(params,catelogId);
 
         return R.ok().put("page", page);
+    }
+
+    ///product/attrgroup/{catelogId}/withattr
+    @GetMapping("/{catelogId}/withattr")
+    public R getAttrGroupWithAttrs(@PathVariable("catelogId") Long catelogId){
+        //1.查出当前分类下的所有属性分组
+        //2.查出每个分组下的所有属性
+        List<AttrGroupWithAttrsVo> vos = attrGroupService.getAttrGroupWithAttrsByCatelogId(catelogId);
+        return R.ok().put("data",vos);
     }
 
 
