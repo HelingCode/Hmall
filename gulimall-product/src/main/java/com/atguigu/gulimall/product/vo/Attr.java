44c38967415bed796edc76354f87ddf32c7d7d59
HelingCode <929926540@qq.com>
2021-11-07 16:38:15 +0800

    新增商品-商品新增vo抽取

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/Attr.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2021 json.cn
+ */
+package com.atguigu.gulimall.product.vo;
+
+/**
+ * Auto-generated: 2021-11-07 16:13:48
+ *
+ * @author json.cn (i@json.cn)
+ * @website http://www.json.cn/java2pojo/
+ */
+public class Attr {
+
+    private int attrId;
+    private String attrName;
+    private String attrValue;
+
+    public void setAttrId(int attrId) {
+        this.attrId = attrId;
+    }
+
+    public int getAttrId() {
+        return attrId;
+    }
+
+    public void setAttrName(String attrName) {
+        this.attrName = attrName;
+    }
+
+    public String getAttrName() {
+        return attrName;
+    }
+
+    public void setAttrValue(String attrValue) {
+        this.attrValue = attrValue;
+    }
+
+    public String getAttrValue() {
+        return attrValue;
+    }
+
+}
---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/Images.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2021 json.cn
+ */
+package com.atguigu.gulimall.product.vo;
+
+/**
+ * Auto-generated: 2021-11-07 16:13:48
+ *
+ * @author json.cn (i@json.cn)
+ * @website http://www.json.cn/java2pojo/
+ */
+public class Images {
+
+    private String imgUrl;
+    private int defaultImg;
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setDefaultImg(int defaultImg) {
+        this.defaultImg = defaultImg;
+    }
+
+    public int getDefaultImg() {
+        return defaultImg;
+    }
+
+}
